796f4600fae1b3540e53c355a14b802e712190cb
ab25cq <ab25cq@icloud.com>
2024-11-25 21:52:58 +0900

    ii

--- X/a.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char** argv)
-{
-    puts("HELLO WORLD");
-    return 0;
-}
--- X/common.h
+++
@@ -1,7 +0,0 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
-#include <comelang.h>
-int main(int argc, char** argv);
-
-
-#endif
