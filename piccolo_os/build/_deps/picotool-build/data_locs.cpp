09b934fae596abcd449b19a9cfadde5e6e462fea
ab25cq <ab25cq@icloud.com>
2024-12-12 17:17:56 +0900

    iii

---
+++ piccolo_os/build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/Users/ab25cq/comelang-dev/pico/piccolo_os_v1/build/_deps/picotool/"
+};
---
+++ piccolo_os/build/_deps/picotool-build/errors/CMakeFiles/errors.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/errors.dir/errors.cpp.o"
+  "CMakeFiles/errors.dir/errors.cpp.o.d"
+  "liberrors.a"
+  "liberrors.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/errors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
