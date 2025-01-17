488890f21185a5b465033ec1b30b29ede4b2b617
ab25cq <ab25cq@icloud.com>
2025-01-17 15:24:26 +0900

    ii

--- pcre.org/all_build.sh
+++ pcre.org/all_build.sh
@@ -1,3 +1,3 @@
-./configure --disable-shared --disable-jit --disable-stack-for-recursion --disable-pcregrep-libz --disable-pcregrep-libbz2 --disable-pcretest-libreadline
+./configure --disable-shared --disable-jit --disable-stack-for-recursion --disable-pcregrep-libz --disable-pcregrep-libbz2 --disable-pcretest-libreadline --disable-pcre16 --disable-pcre32 --disable-pcrecpp
 #mkdir -p build
 (cd build; cmake .. && make)
--- pcre/all_build.sh
+++ pcre/all_build.sh
@@ -1,3 +1,4 @@
-./configure --disable-shared --disable-jit --disable-stack-for-recursion --disable-pcregrep-libz --disable-pcregrep-libbz2 --disable-pcretest-libreadline
-#mkdir -p build
+./configure --disable-shared --disable-jit --disable-stack-for-recursion --disable-pcregrep-libz --disable-pcregrep-libbz2 --disable-pcretest-libreadline --disable-pcre16 --disable-pcre32 --disable-pcrecpp
+rm -rf build
+mkdir -p build
 (cd build; cmake .. && make)
