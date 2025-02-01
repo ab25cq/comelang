fb5b39b838711701953da840cedbf9fb7f71c64f
ab25cq <ab25cq@icloud.com>
2025-02-01 14:13:45 +0900

    iii

--- install_pkg.sh
+++ install_pkg.sh
@@ -1,12 +1,12 @@
 
-# alpine linux
-
+# Mac
 if uname | grep Darwin
 then
     brew install git pcre readline ncurses make autoconf libgc readline openssl mysql-client mysql
+# alpine linux
 elif which apk > /dev/null
 then
-    sudo apk add sudo git clang gcc pcre-dev readline-dev ncurses-dev make samurai autoconf valgrind gdb lldb musl-dev gc-dev readline-dev openssl-dev mariadb-connector-c-dev mariadb-client mariadb libdwarf-dev elfutils-dev
+    sudo apk add sudo git clang gcc pcre-dev readline-dev ncurses-dev make autoconf valgrind gdb lldb musl-dev gc-dev readline-dev openssl-dev mariadb-connector-c-dev mariadb-client mariadb libdwarf-dev elfutils-dev
 # termux
 elif test `uname -o` = Android
 then
@@ -15,6 +15,7 @@ then
 elif uname -a | grep Debian
 then
     sudo apt install clang gcc libpcre3-dev libreadline-dev ncurses-dev make autoconf valgrind gdb lldb libgc-dev libssl-dev libmariadb-dev-compat libmariadb-dev mariadb-server mariadb-client libdwarf-dev libelf-dev libc6-dev
+# raspberry pi
 elif uname -a | grep raspberry
 then
     sudo apt install clang gcc libpcre3-dev libreadline-dev ncurses-dev make autoconf valgrind gdb lldb libgc-dev libssl-dev libmariadb-dev-compat libmariadb-dev mariadb-server mariadb-client libdwarf-dev libelf-dev libc6-dev
