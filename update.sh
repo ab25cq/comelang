a7f7e80cead7b38d74bc7c2a04a04e02bbf658cc
ab25cq <ab25cq@icloud.com>
2025-02-21 05:17:45 +0000

    iii

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ sh remove_all_build.sh
 git remote add origin git@github.com:ab25cq/comelang.git
 git add .
 git commit 
-git branch -M master
-git push --force master master
+#git branch -M master
+git push --force origin master
 
 
