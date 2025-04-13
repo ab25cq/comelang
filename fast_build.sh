5f90a87d2bedc66afe53182abc2417941e597624
ab25cq <ab25cq@icloud.com>
2025-04-14 08:34:53 +0900

    iii

--- fast_build.sh
+++ fast_build.sh
@@ -4,10 +4,10 @@ export RM="rm -rf"
 
 if uname -a | grep Android
 then
-    make CC=gcc DESTDIR=$HOME CFLAGS_OPT="-O2 -D__ANDROID__" && make DESTDIR=$HOME install
+    make CC=gcc DESTDIR=$HOME CFLAGS_OPT="-O2 -D__ANDROID__" -j$(($(nproc) /2)) && make DESTDIR=$HOME install
 elif uname -a | grep Darwin
 then
     make CC=clang CFLAGS_OPT="-O2 -D__MAC__" -j$(sysctl -n hw.logicalcpu) && sudo make install
 else
-    make CC=clang CFLAGS_OPT="-O2 -D__LINUX__" -j$(nproc) && sudo make install
+    make CC=clang CFLAGS_OPT="-O2 -D__LINUX__" -j$(($(nproc) / 2)) && sudo make install
 fi
--- self-host.sh
+++ self-host.sh
@@ -5,10 +5,10 @@ export RM="rm -rf"
 
 if uname -a | grep Android
 then
-    make DESTDIR=$HOME CFLAGS_OPT="-O2" comelang && make DESTDIR=$HOME install
+    make DESTDIR=$HOME CFLAGS_OPT="-O2" comelang $(($(nproc) / 2)) && make DESTDIR=$HOME install
 elif uname -a | grep Darwin
 then
-    make CFLAGS_OPT="-O2 -g" comelang -j$(($(sysctl -n hw.logicalcpu) /2)) && sudo make install
+    make CFLAGS_OPT="-O2 -g" comelang -j$(($(sysctl -n hw.logicalcpu) / 2)) && sudo make install
 else
-    make CFLAGS_OPT="-O2 -g" comelang -j$(($(nproc) /2)) && sudo make install
+    make CFLAGS_OPT="-O2 -g" comelang -j$(($(nproc) / 2)) && sudo make install
 fi
