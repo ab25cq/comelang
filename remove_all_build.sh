2b425335b2261e7aac268dd343913ac727f662ef
ab25cq <ab25cq@icloud.com>
2025-04-22 14:13:48 +0900

    ii

--- remove_all_build.sh
+++ remove_all_build.sh
@@ -28,6 +28,7 @@ fi
 (cd sd; rm -rf build)
 (cd pcre; rm -rf build)
 (cd minux3; make clean)
+(cd minux5; make clean)
 (cd webweb; sh clean_build.sh)
 (cd webweb/cgi-bin; make clean)
 (cd xv6-riscv; make clean)
