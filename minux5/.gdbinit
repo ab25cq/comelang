888a76d0a3d0dfd6096770fbc0ef258d511f4756
ab25cq <ab25cq@icloud.com>
2025-04-23 10:02:21 +0900

    iii

--- minux5/.gdbinit
+++ minux5/.gdbinit
@@ -1,7 +1,9 @@
+file kernel.elf
+dir /root/comelang/minux5
 target remote localhost:3777
 set architecture riscv:rv64
 set disassemble-next-line on
 layout asm
 layout src
 layout regs
-b _start
+b main
--- minux5/main.c
+++ minux5/main.c
@@ -1,9 +1,78 @@
+//#include <stdarg.h>
 
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+typedef unsigned char  uchar;
 
-volatile char *uart = (char *)0x10000000;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int  uint32;
+typedef unsigned long uint64;
+
+typedef uint64 pde_t;
+
+static char digits[] = "0123456789ABCDEF";
+
+struct context 
+{
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
+struct cpu 
+{
+    struct proc *proc;          // The process running on this cpu, or null.
+    struct context context;     // swtch() here to enter scheduler().
+};
+
+struct cpu cpu;
+
+enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+
+// Per-process state
+struct proc 
+{
+  enum procstate state;        // Process state
+  struct context context;      // swtch() here to run process
+
+  struct proc *parent;         // Parent process
+  
+  char* stack[256];
+};
+
+#define KERNBASE 0x80000000L
+#define PHYSTOP (KERNBASE + 128*1024*1024)
+
+#define PGSIZE 4096 // bytes per page
+#define PGROUNDUP(sz)  (((sz)+PGSIZE-1) & ~(PGSIZE-1))
+#define PGROUNDDOWN(a) (((a)) & ~(PGSIZE-1))
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct run *freelist;
+} kmem;
 
 void putc(char c) {
-
     *(volatile char*)(0x10000000) = c;
 }
 
@@ -11,9 +80,9 @@ void puts(const char *s) {
     while (*s) putc(*s++);
 }
 
-
 int main()
 {
-    puts("HELLO WORLD\n");
+    putc('a');
+//puts("HELLO WORLD");
     while (1);
 }
