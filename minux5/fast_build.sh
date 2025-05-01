c4cbb1ee65b673ddb208ee8550ac1ae24617d43a
ab25cq <ab25cq@icloud.com>
2025-05-02 05:24:35 +0900

    iii

--- minux5/fast_build.sh
+++ minux5/fast_build.sh
@@ -27,5 +27,20 @@ then
     echo "add-auto-load-safe-path $(pwd)/.gdbinit" >> ~/.config/gdb/gdbinit
 fi
 
+if which riscv-none-elf-gcc
+then
+    echo "OK"
+else
+    curl -LO https://github.com/xpack-dev-tools/riscv-none-elf-gcc-xpack/releases/download/v13.2.0-1/xpack-riscv-none-elf-gcc-13.2.0-1-linux-arm64.tar.gz
+    tar -xvf xpack-riscv-none-elf-gcc-*.tar.gz
+    sudo mkdir -p /opt/riscv
+    sudo mv xpack-riscv-none-elf-gcc-* /opt/riscv
+    echo 'export PATH=/opt/riscv/xpack-riscv-none-elf-gcc-13.2.0-1/bin:$PATH' >> ~/.bashrc
+    export PATH=/opt/riscv/xpack-riscv-none-elf-gcc-13.2.0-1/bin:$PATH
+fi
+
+
 make clean
-make debug
+#make debug CCPREFIX=riscv-none-elf- CFLAGS="-march=rv64gc -mabi=lp64"
+make debug CCPREFIX=riscv64-unknown-elf- CFLAGS=""
+
