4ba6a65b42c602a4c032c75b7d07a4ee926ae171
ab25cq <ab25cq@icloud.com>
2025-05-21 23:50:27 +0000

    iii

--- self-host.sh
+++ self-host.sh
@@ -3,7 +3,10 @@ sh install_pkg.sh
 
 export RM="rm -rf"
 
-if uname -a | grep Android
+if uname -a | grep raspberry
+then
+    make DESTDIR=$HOME CFLAGS_OPT="-O2 -D__RASPBERRY_PI__" comelang && make DESTDIR=$HOME install
+elif uname -a | grep Android
 then
     make DESTDIR=$HOME CFLAGS_OPT="-O2" comelang $(($(nproc) / 2)) && make DESTDIR=$HOME install
 elif uname -a | grep Darwin
