6764f4f10d313b8458f544559b8f0b1128eb922f
ab25cq <ab25cq@icloud.com>
2025-06-24 10:33:03 +0900

    iii

--- minux10/.gdbinit
+++
@@ -1,7 +0,0 @@
-file kernel.elf
-target remote :1234
-dir .
-layout asm
-layout src
-layout regs
-list
--- minux11/fast_build.sh
+++
@@ -1,7 +0,0 @@
-make clean
-
-rm -rf ~/.config/gdb
-mkdir -p ~/.config/gdb
-echo "add-auto-load-safe-path $(pwd)/.gdbinit" >> ~/.config/gdb/gdbinit
-
-make debug
