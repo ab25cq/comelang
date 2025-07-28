44136d6f42180699befdf615c5a32bc5ed9d6642
ab25cq <ab25cq@icloud.com>
2025-07-28 11:23:32 +0900

    ii

--- a.c
+++ a.c
@@ -3,7 +3,7 @@
 int main(int argc, char** argv)
 {
     int c = 1;
-    int a[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int a[3][3] = { c, 2, 3, 4, 5, 6, 7, 8, 9};
     
     int** b = a[2] + 1;
     
