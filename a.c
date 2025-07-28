1b854e21d74eef225dbcba749f3f96e055597c09
ab25cq <ab25cq@icloud.com>
2025-07-28 09:24:22 +0900

    ii

--- a.c
+++ a.c
@@ -5,7 +5,7 @@ int main(int argc, char** argv)
     int c = 1;
     int a[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
     
-    int* b = a[2] + 1;
+    int** b = a[2] + 1;
     
     printf("%d\n", *b);
     
