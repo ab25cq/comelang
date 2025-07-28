4fc6d22c736e9e91cfe520d7f1a0a36b3a435474
ab25cq <ab25cq@icloud.com>
2025-07-28 17:28:17 +0900

    ii

--- a.c
+++ a.c
@@ -3,11 +3,15 @@
 int main(int argc, char** argv)
 {
     int c = 1;
-    int a[3][3] = { c, 2, 3, 4, 5, 6, 7, 8, 9};
+output {
+    long x;
+    asm volatile("csrr %0, time" : "=r" (x) );
+    guho
+}
     
-    int** b = a[2] + 1;
+//    int** b = a[2] + 1;
     
-    printf("%d\n", *b);
+//    printf("%d\n", *b);
     
     return 0;
 }
