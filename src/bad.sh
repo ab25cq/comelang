987f08291221eb7a7ea595f1c8948f2b0b6306ea
ab25cq <ab25cq@icloud.com>
2025-08-24 17:03:09 +0900

    ii

--- src/bad.sh
+++ src/bad.sh
@@ -1,6 +1,3 @@
-./cparse test/t_more_casts.c
-./cparse test/t_ptr_levels.c
-./cparse test/tmp_enum.c
-./cparse test/tmp_enum_expr.c
-./cparse test/tmp_named_enum.c
-./cparse test/typedefs.c
+./cparse test/tmp_const.c
+./cparse test/tmp_varuniq.c
+./cparse test/tmp_varuniq2.c
