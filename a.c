1c36b1bfc9c1f4312b77d5fb4c5f2cf185f418d3
ab25cq <ab25cq@icloud.com>
2025-09-02 10:45:44 +0900

    ii

--- a.c
+++ a.c
@@ -4,5 +4,6 @@ int main(int argc, char** argv) {
     int i = 3;
     int j = 7;
     int m = ({ __typeof__(i++) _a = (i++); __typeof__(j++) _b = (j++); _a > _b ? _a : _b; }); // both evaluated exactly once
+    printf("m %d\n", m);
     return 0;
 }
