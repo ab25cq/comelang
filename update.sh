fba23630129f0cf40fe1be4b74b37ceb2a779973
ab25cq <ab25cq@icloud.com>
2025-11-13 18:04:28 +0900

    reborn

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ sh remove_all_build.sh
 
 git add .
 git commit 
+git remote add origin git@github.com:ab25cq/neo-c.git
 git remote set-url origin git@github.com:ab25cq/neo-c.git
 git push --force origin master
 
